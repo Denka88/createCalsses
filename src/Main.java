96ef1bcf0210a5659f1561ad7507cc205be876af
Denka88 <shadowfightdenka88@gmail.com>
2024-10-10 14:42:10 +0300

    Доделать задачу 3 и далее

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Vector firstVector = new Vector(8, 6, 4);
-        Vector secondVector = new Vector();
+        Vector firstVector = new Vector(2, 2, 2);
+        Vector secondVector = new Vector(2, 2, 2);
 
-        secondVector.setX(4);
-        secondVector.setY(6);
-        secondVector.setZ(8);
-
-        scalar(firstVector, secondVector);
-
-    }
-
-    public static int scalar(Vector first, Vector second){
-
-        return first.getX() * second.getX() + first.getY() * second.getY() + first.getZ() * second.getZ();
+        System.out.println(secondVector.lengthVector());
+        System.out.println(secondVector);
+        System.out.println(Vector.scalar(firstVector, secondVector));
 
     }
 }
--- src/Vector.java
+++ src/Vector.java
@@ -1,10 +1,10 @@
 public class Vector {
 
-    private static int x;
-    private static int y;
-    private static int z;
+    private int x;
+    private int y;
+    private int z;
 
-    public Vector(){
+    public Vector() {
 
     }
 
@@ -38,10 +38,46 @@ public class Vector {
         this.z = z;
     }
 
-    public static int lenghtVector() {
+
+    //    Длина вектора Задача 1
+    public int lengthVector() {
 
         return (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
 
     }
 
+    public static String scalar(Vector first, Vector second) {
+        System.out.printf("Vector{%d, %d, %d}", first.getX(), first.getY(), first.getZ());
+        return "Vector scalar{ " + first.getX() * second.getX() + first.getY() * second.getY() + first.getZ() * second.getZ() + "}";
+
+    }
+
+    /**
+     * Метод для вычисления векторного произведения двух векторов
+     *
+     * @param first  Первый вектор
+     * @param second Второй вектор
+     * @return Возврпает векторное произведение двух векторов
+     */
+    public Vector vectorMult(Vector first, Vector second) {
+
+        int a = first.getY() * second.getZ() - first.getZ() * second.getY();
+        int b = first.getZ() * second.getX() - first.getX() * second.getZ();
+        int c = first.getX() * second.getY() - first.getY() * second.getX();
+
+        System.out.printf(new Vector(a, b, c) + "Vector{%d, %d, %d}", a, b, c);
+
+        return new Vector(a, b, c);
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
 }
